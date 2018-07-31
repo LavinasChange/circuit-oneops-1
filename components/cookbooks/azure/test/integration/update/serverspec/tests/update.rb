4eb4ef1e04212c9597c58d4e75e5de480170c371
Maham Nazir <maham.nazir@confiz.com>
2018-07-31 12:01:08 +0500

    Fixed KCI for Azure Touch/Update
    Summary: The KCI was failing as it was trying to match both private and public IPs. It has been modified to match IPs based on the IP type: private or public

--- components/cookbooks/azure/test/integration/update/serverspec/tests/update.rb
+++ components/cookbooks/azure/test/integration/update/serverspec/tests/update.rb
@@ -20,27 +20,34 @@ describe 'azure node::update' do
   end
 
   context 'Compute' do
-    it 'should have the same public & private IP address' do
-      public_ip_node = $node['workorder']['rfcCi']['ciAttributes']['public_ip']
-      private_ip_node = $node['workorder']['rfcCi']['ciAttributes']['private_ip']
-
+    it 'should have the same public/private IP address' do
       rg_svc = AzureBase::ResourceGroupManager.new($node)
       ci_id = $node['workorder']['rfcCi']['ciId']
 
-      nic_svc = AzureNetwork::NetworkInterfaceCard.new(@spec_utils.get_azure_creds)
-      nic_svc.ci_id = ci_id
-      nic_svc.platform_ci_id = $node['workorder']['box']['ciId'] if Utils.is_new_cloud($node)
-      nic_svc.rg_name = rg_svc.rg_name
-      nic_name = Utils.get_component_name('nic', nic_svc.ci_id, nic_svc.platform_ci_id)
-      nic = nic_svc.get(nic_name)
+      cloud_name = $node['workorder']['cloud']['ciName']
+      compute_service = $node['workorder']['services']['compute'][cloud_name]['ciAttributes']
+      ip_type = (compute_service['express_route_enabled'].eql? 'true') ? 'private' : 'public'
 
-      pip = AzureNetwork::PublicIp.new(@spec_utils.get_azure_creds)
-      public_ip_name = Utils.get_component_name('publicip', ci_id)
-      publicip_details = pip.get(rg_svc.rg_name, public_ip_name)
-      pubip_address = publicip_details.ip_address
+      if ip_type.eql? 'public'
+        public_ip_node = $node['workorder']['rfcCi']['ciAttributes']['public_ip']
 
-      expect(public_ip_node).to eq(pubip_address)
-      expect(private_ip_node).to eq(nic.private_ip_address)
+        pip = AzureNetwork::PublicIp.new(@spec_utils.get_azure_creds)
+        public_ip_name = Utils.get_component_name('publicip', ci_id)
+        public_ip = pip.get(rg_svc.rg_name, public_ip_name)
+
+        expect(public_ip_node).to eq(public_ip.ip_address)
+      else
+        private_ip_node = $node['workorder']['rfcCi']['ciAttributes']['private_ip']
+
+        nic_svc = AzureNetwork::NetworkInterfaceCard.new(@spec_utils.get_azure_creds)
+        nic_svc.ci_id = ci_id
+        nic_svc.platform_ci_id = $node['workorder']['box']['ciId'] if Utils.is_new_cloud($node)
+        nic_svc.rg_name = rg_svc.rg_name
+        nic_name = Utils.get_component_name('nic', nic_svc.ci_id, nic_svc.platform_ci_id)
+        nic = nic_svc.get(nic_name)
+
+        expect(private_ip_node).to eq(nic.private_ip_address)
+      end
     end
   end
 end
